1ba28605a2c4fe16d9bfeb78124c62e14771c9d6
akshay1992 <akshay19.92@gmail.com>
2016-05-11 19:12:30 -0700

    First commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,197 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    video.load("marc.mp4");
+    video.play();
+    
+    if(!audio.open_file("ApproxPi_8ChannelNew.wav"))
+    {
+        cout << "Error opening file" << endl;
+        exit();
+    }
+    ofSoundStreamSetup(8, 0);
+
+}
+
+void ofApp::setup_w2() {
+    
+}
+
+void ofApp::setup_w3() {
+    
+}
+
+void ofApp::setup_w4() {
+    
+}
+
+void ofApp::setup_w5() {
+    
+}
+
+void ofApp::setup_w6() {
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    video.update();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+//    video.draw(0, 0, 400, 300);
+//    video.draw(0, 0, 960, 512);
+//    unsigned char* pixels = video.getPixels();
+    
+    ofTexture& tex = video.getTexture();
+    
+    tex.drawSubsection(0, 0, 320, 256, 0, 0, 320, 256);
+    
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.0f, 0.0f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(0.33f, 0.0f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(0.33f, 0.5f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.0f, 0.5f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();  
+    
+}
+
+void ofApp::draw_w2(ofEventArgs & args){
+    ofTexture& tex = video.getTexture();
+    
+    tex.drawSubsection(0, 0, 320, 256, 320, 0, 320, 256);
+
+    
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.33f, 0.0f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(0.66f, 0.0f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(0.66f, 0.5f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.33f, 0.5f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();
+}
+
+void ofApp::draw_w3(ofEventArgs & args){
+    ofTexture& tex = video.getTexture();
+    tex.drawSubsection(0, 0, 320, 256, 640, 0, 320, 256);
+
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.66f, 0.0f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(1.0f, 0.0f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(1.0f, 0.5f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.66f, 0.5f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();
+}
+void ofApp::draw_w4(ofEventArgs & args){
+    ofTexture& tex = video.getTexture();
+    tex.drawSubsection(0, 0, 320, 256, 0, 256, 320, 256);
+    
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.0f, 0.5f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(0.33f, 0.5f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(0.66f, 1.0f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.0f, 1.0f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();
+}
+void ofApp::draw_w5(ofEventArgs & args){
+    ofTexture& tex = video.getTexture();
+    tex.drawSubsection(0, 0, 320, 256, 320, 256, 320, 256);
+
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.33f, 0.5f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(0.66f, 0.5f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(0.66f, 1.0f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.33f, 1.0f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();
+}
+void ofApp::draw_w6(ofEventArgs & args){
+    ofTexture& tex = video.getTexture();
+    tex.drawSubsection(0, 0, 320, 256, 640, 256, 320, 256);
+    
+//    tex.bind();
+//    glBegin(GL_QUADS);
+//    glTexCoord2f(0.66f, 0.5f); glVertex2f(0.0f,0.0f);
+//    glTexCoord2f(1.0f, 0.5f); glVertex2f(320.0f,0.0f);
+//    glTexCoord2f(1.0f, 1.0f); glVertex2f(320.0f,256.0f);
+//    glTexCoord2f(0.66f, 1.0f); glVertex2f(0.0f,256.0f);
+//    glEnd();
+//    tex.unbind();
+}
+
+//--------------------------------------------------------------
+void ofApp::audioOut( float * output, int bufferSize, int nChannels ) {
+    for (int i=0; i<bufferSize * nChannels; i+=nChannels)
+    {
+        for (int chan=0; chan<nChannels; chan++)
+            {
+            output[i+chan] = audio.audiofile.next_sample();
+            }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ofMain.h"
+#include "WavPlayer.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+    void setup_w2();
+    void setup_w3();
+    void setup_w4();
+    void setup_w5();
+    void setup_w6();
+    
+		void update();
+		void draw();
+    void draw_w2(ofEventArgs & args);
+    void draw_w3(ofEventArgs & args);
+    void draw_w4(ofEventArgs & args);
+    void draw_w5(ofEventArgs & args);
+    void draw_w6(ofEventArgs & args);
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    void audioOut( float * output, int bufferSize, int nChannels );
+        ofVideoPlayer video;
+    
+    WavPlayer audio;
+};
